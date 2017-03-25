ad0aa9f447a82fbf05ef29ea0a986e81e54130b8
wangyunchuan <wangyunchuan3410@163.com>
2017-03-25 18:50:17 +0800

    添加源文件

---
+++ project_full/src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/3/25 0025.
+ */
+(function() {
+    function indexHandler () {
+            var a= 0;
+            console.log("a");
+            return a;
+    }
+    indexHandler();
+})();
